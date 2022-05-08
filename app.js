e94705d1a96df1a410221450b7bd6ee90e28a034
eduardo-solis <solishernandezeduardoruben@gmail.com>
2022-05-08 14:41:58 -0500

    Paso 3: Contestando preguntas

--- app.js
+++ app.js
@@ -19,3 +19,33 @@ trello.addCard(cardTitle, "LaunchX Card 2 Description", "6277f777474b1c1ab7ceefd
       console.log('Added card:', trelloCard);
     }
   });
+
+/*
+Qué dependencias estan usando.
+
+1. es6-promise
+2. object-assign
+3. needle -> se cambio a restler
+4. chai
+5. mocha
+6. q
+7. sinon
+8. sinon-chai
+
+Cuál es el archivo principal
+
+- El archivo principal es el main.js
+
+Están usando Common JS o ESM
+
+- En el proyecto se usa Common JS
+
+Qué framework de pruebas estás usando
+
+Se esta usando Chai, Sinon y Mocha
+
+Cómo están diseñadas las pruebas
+
+Primero tienen su describe, y en dentro de cada prueba se crea primero un foreachBefore, seguido de eso se 
+hacen las pruebas usando la palabra clave "it"
+*/
